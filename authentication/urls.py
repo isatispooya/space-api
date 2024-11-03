d85d9e7489330d263e4d89249013c847bda33478
moeendehqan <moeen.dehqan@gmail.com>
2024-11-03 13:46:25 +0330

    group

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,15 +1,24 @@
-from .views import  CaptchaViewset
+from .views import  CaptchaViewset, GroupManagementViewSet
 from django.urls import path
 from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import CustomTokenObtainPairView
-
 urlpatterns = [
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('captcha/', CaptchaViewset.as_view(), name='captcha'),
-
+    path('groups/', GroupManagementViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    }), name='group-list'),
+    
+    path('groups/<int:pk>/', GroupManagementViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name='group-detail'),
 ]
 
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from django.utils.decorators import method_decorator
@@ -6,13 +5,14 @@ from django_ratelimit.decorators import ratelimit
 from GuardPyCaptcha.Captch import GuardPyCaptcha
 from rest_framework.response import Response
 from rest_framework import status
-from user import fun
-from user.models import User
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from django.contrib.auth.models import Group
+from .serializer import GroupSerializer
+
 
 class CaptchaViewset(APIView) :
     permission_classes = [AllowAny]
@@ -22,28 +22,48 @@ class CaptchaViewset(APIView) :
         captcha = captcha.Captcha_generation(num_char=4 , only_num= True)
         return Response ({'captcha' : captcha} , status = status.HTTP_200_OK)
     
+class GroupManagementViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAdminUser]
 
-class CustomTokenObtainPairView(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
-        password = request.data.get('password')
+    def create(self, request, *args, **kwargs):
+        try:
+            name = request.data.get('name')
+            permissions = request.data.get('permissions', [])
 
-        print(username,password)
-        
-        # تلاش برای احراز هویت
-        user = authenticate(username=username, password=password)
-        print(user)
-        
-        if user is None:
-            return Response(
-                {
-                    "detail": "اطلاعات کاربری نادرست است",
-                    "debug_info": {
-                        "username_received": username,
-                        "password_received": "***"
-                    }
-                },
-                status=status.HTTP_401_UNAUTHORIZED
-            )
+            group = Group.objects.create(name=name)
             
-        return super().post(request, *args, **kwargs) 
+            if permissions:
+                group.permissions.set(permissions)
+            serializer = self.get_serializer(group)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response(
+                {'error': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            name = request.data.get('name')
+            permissions = request.data.get('permissions', [])
+
+            if name:
+                instance.name = name
+            
+            if permissions:
+                instance.permissions.set(permissions)
+            
+            instance.save()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+
+        except Exception as e:
+            return Response(
+                {'error': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
