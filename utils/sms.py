72fcac242227bb24b0bfea076fd71a872d22fe40
Mahya Abouei <mahyaaboueii@gmail.com>
2024-11-13 12:25:35 +0330

    sms for forgein password

--- utils/sms.py
+++ utils/sms.py
@@ -6,7 +6,7 @@ psswrd ='5246043adeleh'
 
 # sms for uuid
 def SendSmsUUid(snd,txt):
-    txt = f'اتوماسیون اداری ایساتیس پویا\n فراموشی رمز عبور:\n https://isatispooya.com/{txt}/'
+    txt = f'اتوماسیون اداری ایساتیس پویا\n فراموشی رمز عبور:\n https://space.isatispooya.com/reset-password/{txt}/'
     resp = requests.get(url=f'http://tsms.ir/url/tsmshttp.php?from={frm}&to={snd}&username={usrnm}&password={psswrd}&message={txt}').json()
     print(txt)
     return resp
