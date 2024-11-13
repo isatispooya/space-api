d9c375365882828db13215e150ae88c76643bd3e
moeendehqan <moeen.dehqan@gmail.com>
2024-11-13 12:55:17 +0330

    position view done

--- positions/views.py
+++ positions/views.py
@@ -5,12 +5,10 @@ from rest_framework.permissions import AllowAny , IsAuthenticated  , IsAdminUser
 from .serializers import PositionSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 
-
-class PositionViewset(APIView):
+class PositionViewset(viewsets.ModelViewSet):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
     permission_classes = [IsAuthenticated]
-    def post(self, request):
-
-        pass 
-
