422cc1cacb5303fafc91c18f79b32d57e575fcc2
Mahya Abouei <mahyaaboueii@gmail.com>
2024-12-16 10:24:38 +0330

    core models / admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Announcement , ShortCut
 
-# Register your models here.
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'link' , 'is_active']
+    list_filter = ['created_at' , 'is_active', 'title']
+    search_fields = ['title']   
+    fieldsets = [
+        ('اطلاعات اصلی', {
+            'fields': ['title' , 'description' , 'picture' , 'link' , 'is_active']
+        }),
+        ('تاریخ‌ها', {
+            'fields': ['created_at' , 'updated_at']
+        })
+    ]
+
+
+@admin.register(ShortCut)
+class ShortCutAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'link' , 'is_active']
+    list_filter = ['created_at' , 'is_active', 'title']
+    search_fields = ['title']
+    fieldsets = [
+        ('اطلاعات اصلی', {
+            'fields': ['title' , 'link' , 'is_active']
+        }),
+        ('تاریخ‌ها', {
+            'fields': ['created_at' , 'updated_at']
+        })
+    ]
