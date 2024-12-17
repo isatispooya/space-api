ff96028256a001d58643e28bfc9fcd8e16f15414
Mahya Abouei <mahyaaboueii@gmail.com>
2024-12-17 14:59:11 +0330

    correspondence menu

--- core/views.py
+++ core/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
 from .models import Announcement , ShortCut 
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny , IsAdminUser , IsAuthenticated
 from .serializers import AnnouncementSerializer , ShortCutSerializer 
 from rest_framework.response import Response
-from stock_affairs.models import Shareholders , Precedence , UnusedPrecedencePurchase
 from stock_affairs.permission import IsShareholder , IsPrecedence , IsUnusedPrecedencePurchase
 
 
@@ -45,19 +43,25 @@ class MenuView(APIView):
     def menu_stock_affairs(self, request):
         main = {'title': 'امور سهام','path': '/stock_affairs'}
         sub_menu = []
+
         if IsShareholder().has_permission(request, self):
             sub_menu.append({'title': 'سهام','path': '/stock_affairs/stock'})
+
         if IsPrecedence().has_permission(request, self):
             sub_menu.append({'title': 'حق تقدم','path': '/stock_affairs/precedence'})
+
         if IsUnusedPrecedencePurchase().has_permission(request, self):
             sub_menu.append({'title': 'خرید حق تقدم استفاده نشده','path': '/stock_affairs/unused_precedence_purchase'})
+
         if len(sub_menu)>0:
             main['sub_menu'] = sub_menu
             return main
         else:
             return None
 
-    
+    def menu_correspondence(self, request):
+        return {'title': 'مکاتبات', 'path': '/correspondence'}
+
     def get(self, request):
         self.menu_items = []
         # home
