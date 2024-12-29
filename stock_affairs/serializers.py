4341084bddedf7bb021fa8ecfe14b552a7189a0f
Mahya Abouei <mahyaaboueii@gmail.com>
2024-12-29 12:08:14 +0330

    Process Description

--- stock_affairs/serializers.py
+++ stock_affairs/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Shareholders , StockTransfer , Precedence , CapitalIncreasePayment , DisplacementPrecedence , Underwriting , UnusedPrecedenceProcess , Appendices
+from .models import Shareholders , StockTransfer , Precedence , CapitalIncreasePayment , DisplacementPrecedence , Underwriting , UnusedPrecedenceProcess , Appendices , ProcessDescription
 from companies.serializers import CompanySerializer
 from user.serializers import UserSerializer
 from transactions.serializers import PaymentSerializer
@@ -62,11 +62,17 @@ class AppendicesSerializer(serializers.ModelSerializer):
         model = Appendices
         fields = '__all__'
 
+class ProcessDescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessDescription
+        fields = '__all__'
+
 class UnderwritingSerializer(serializers.ModelSerializer):
     appendices = AppendicesSerializer(read_only=True)
     process_detail = UnusedPrecedenceProcessSerializer(source='unused_precedence_process', read_only=True)
     payment_detail = PaymentSerializer(source='payment', read_only=True)
     user_detail = UserSerializer(source='user', read_only=True)
+    process_description = ProcessDescriptionSerializer(source='process_description', read_only=True)
 
     class Meta:
         model = Underwriting
