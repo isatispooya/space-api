596eb6ad0c69ad6341d8e1f50f8a34df8f9e7f37
Mahya Abouei <mahyaaboueii@gmail.com>
2024-12-29 09:16:13 +0330

    Appendices model

--- stock_affairs/admin.py
+++ stock_affairs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Shareholders, StockTransfer, DisplacementPrecedence, CapitalIncreasePayment , Precedence , Underwriting , UnusedPrecedenceProcess
+from .models import Shareholders, StockTransfer, DisplacementPrecedence, CapitalIncreasePayment , Precedence , Underwriting , UnusedPrecedenceProcess, Appendices
 
 
 admin.site.register(Shareholders)
@@ -9,4 +9,5 @@ admin.site.register(CapitalIncreasePayment)
 admin.site.register(Precedence)
 admin.site.register(Underwriting)
 admin.site.register(UnusedPrecedenceProcess)
+admin.site.register(Appendices)
 
