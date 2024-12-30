7985e050d03f199b1c5de78ff92338e27e119413
Mahya Abouei <mahyaaboueii@gmail.com>
2024-12-30 11:01:07 +0330

    core admin

--- core/admin.py
+++ core/admin.py
@@ -6,26 +6,22 @@ class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ['title' , 'link' , 'is_active']
     list_filter = ['created_at' , 'is_active', 'title']
     search_fields = ['title']   
+    readonly_fields = ['created_at', 'updated_at']
     fieldsets = [
         ('اطلاعات اصلی', {
             'fields': ['title' , 'description' , 'picture' , 'link' , 'is_active']
         }),
-        ('تاریخ‌ها', {
-            'fields': ['created_at' , 'updated_at']
-        })
     ]
 
 
 @admin.register(ShortCut)
 class ShortCutAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'link' , 'is_active']
+    list_display = ['title' , 'link' , 'is_active' ]
     list_filter = ['created_at' , 'is_active', 'title']
     search_fields = ['title']
+    readonly_fields = ['created_at', 'updated_at']
     fieldsets = [
         ('اطلاعات اصلی', {
-            'fields': ['title' , 'link' , 'is_active']
-        }),
-        ('تاریخ‌ها', {
-            'fields': ['created_at' , 'updated_at']
+            'fields': ['title' , 'link' , 'is_active' , 'picture']
         })
     ]
