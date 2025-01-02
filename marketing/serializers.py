f2fc1c8c2e7672afe32c53009b2b3af514770462
Mahya Abouei <mahyaaboueii@gmail.com>
2025-01-02 11:21:28 +0330

    serializer

--- marketing/serializers.py
+++ marketing/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import InvitationCode, Invitation
+from user.serializers import UserSerializer
 
 class InvitationCodeSerializer(serializers.ModelSerializer):
+    introducer_user_detail = UserSerializer(source='introducer_user', read_only=True)
     class Meta:
         model = InvitationCode
         fields = '__all__'
 
 class InvitationSerializer(serializers.ModelSerializer):
+    invitation_code_detail = InvitationCodeSerializer(source='invitation_code', read_only=True)
+    invited_user_detail = UserSerializer(source='invited_user', read_only=True)
     class Meta:
         model = Invitation
         fields = '__all__'
