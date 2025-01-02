4eca5d54a6acc739e402bdb48f373eefb397a269
Mahya Abouei <mahyaaboueii@gmail.com>
2025-01-02 10:48:28 +0330

    marketing

---
+++ marketing/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class InvitationCode(models.Model):
+    code = models.CharField(
+        max_length=6,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name='کد دعوت'
+    )
+    introducer_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='دعوت کننده'
+    )
+    description = models.TextField(
+        verbose_name='توضیحات',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ایجاد'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ بروزرسانی'
+    )
+    class Meta:
+        verbose_name = 'کد دعوت'
+        verbose_name_plural = 'کد های دعوت'
+
+    def __str__(self):
+        return self.code
+
+
+class Invitation(models.Model):
+    invitation_code = models.ForeignKey(
+        InvitationCode,
+        on_delete=models.CASCADE,
+        verbose_name='کد دعوت'
+    )
+    invited_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='کاربر دعوت شده'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ایجاد'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ بروزرسانی'
+    )
+    class Meta:
+        verbose_name = 'دعوت'
+        verbose_name_plural = 'دعوت ها' 
+    
+    def __str__(self):
+        return f'{self.invited_user.username} - {self.invitation_code.code}'
+
+
